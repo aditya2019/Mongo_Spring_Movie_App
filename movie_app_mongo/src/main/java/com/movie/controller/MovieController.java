848fd78ee2108bde9fa34a350d00bf0b6ca59b79
adityapalmehula <javajiadityapal@gmail.com>
2018-03-28 19:05:01 +0530

    5th commit

--- movie_app_mongo/src/main/java/com/movie/controller/MovieController.java
+++ movie_app_mongo/src/main/java/com/movie/controller/MovieController.java
@@ -2,7 +2,9 @@ package com.movie.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -30,15 +32,36 @@ import com.movie.repository.FavouriteMovieRepository;
 	        return movieRepository.findAll();
 	    }
 	    
-	    @RequestMapping(value="/movies",method=RequestMethod.POST,consumes = MediaType.APPLICATION_JSON_VALUE)
-	    public  void addMovie(@RequestBody Movie movie){
-	     movieRepository.save(movie);
-	    	
-	    }
+	    @RequestMapping(value = "/movies", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
+		public ResponseEntity<Void> create(@RequestBody Movie movie) {
+			 try {
+				 movieRepository.save(movie);
+			  return ResponseEntity.noContent().build();
+			 } catch (Exception e) {
+			  return ResponseEntity.status(HttpStatus.CONFLICT).build();
+			 }
+			}
+	    
+
+		@RequestMapping(value = "/updateMovie", method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE)
+		public ResponseEntity<Void> updateExist(@RequestBody Movie movie)  {
+			 try {
+			  movieRepository.save(movie);
+			  return ResponseEntity.noContent().build();
+			 } catch (Exception e) {
+			  return ResponseEntity.notFound().build();
+			 }
+			}
+	    
 	    @RequestMapping(value="/{id}",method=RequestMethod.DELETE,consumes = MediaType.APPLICATION_JSON_VALUE)
-	    public  void delete(@PathVariable Integer id){
-	     movieRepository.deleteById(id);
-	    	
+	    public  ResponseEntity<Void> delete(@PathVariable Integer id){
+	      try {
+	    	movieRepository.deleteById(id);
+	     return ResponseEntity.noContent().build();
+	      }
+	      catch (Exception e) {
+	    	  return ResponseEntity.notFound().build();
+	    	 }
 	    }
 	}
 
